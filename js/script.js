c561587b1f8023a74586629bd5c00e4bbebab1da
scollo26 <giovanniscollo26@gmail.com>
2021-12-03 16:50:23 +0100

    timer2

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ function random(min, max) {
     }
     
     return listNumbers;
-    
+
 }
 
 
@@ -42,18 +42,20 @@ function timer(){
 
     const timer = setInterval(() => {
 
-        if (second < 10) {
+        if (second < 30) {
             second += 1;
             console.log(second);
         } else{
             console.log('tempo finito');
-            clearInterval(timer);
             container.innerHTML ='';
-        }
-
-        
-        
+            clearInterval(timer);
+            
+        }  
     }, 1000);
+
+    
+
+
 }
 
 
@@ -62,14 +64,15 @@ function timer(){
 
 
 
-let container = document.getElementById('container')
+let container = document.getElementById('container');
+let container2 = document.getElementById('container2')
 const numberRandom = random(1, 5);
 console.log(numberRandom);
 
 
 let times = timer();
 
-// let times2 = timer2();
+let times2 = timer2();
 
 container.append(numberRandom);
 
@@ -77,55 +80,56 @@ container.append(numberRandom);
 
 
 
-
-
-let numberUtente = [];
-let utente = [];
-for (let i = 0; i < 5; i++) {
     
-    // utente = parseInt(prompt('inserisci un numero'));
-    numberUtente += utente;
-    console.log(numberUtente[i]);
-    
+function timer2(){
+
+    const timer2 = setTimeout(() => {
+        
+        
+        let utente = [];
+        let listUtente = [];
+        for (let i = 0; i < 5; i++) {
+            
+            utente = parseInt(prompt('inserisci un numero'))
+            listUtente.push(utente);
+            console.log(listUtente);
+            
+        }
+
+        
+        
+
+        let risultato = [];
+        let risultatoSbagliato = [];
+        let vero = 0;
+        let falso = 0;
+        
+        for (let i = 0; i < 5; i++) {
+            console.log(numberRandom);
+
+            
+            if(!(listUtente[i] == numberRandom[i])){
+                console.log('sbagliato');
+                // container.innerHTML ='non hai memorizato tutti i numeri';
+                risultatoSbagliato.push(listUtente[i]);
+                falso ++
+
+            }else{
+                risultato.push(listUtente[i]);
+                vero ++;
+
+            }
+            
+        
+            
+        }
+        container.innerHTML='numeri giusti ' + vero + '.... ' + risultato;
+        container2.innerHTML='numeri sbagliati ' + falso + '.... ' + risultatoSbagliato;
+        
+        console.log(risultato);
+        console.log(risultatoSbagliato);
+
+        
+        clearInterval(timer2);
+    }, 32000);
 }
-
-
-for (let i = 0; i < 5; i++) {
-    if(!(numberUtente[i] == numberRandom[i])){
-        console.log('sbagliato');
-    }
-    
-}
-
-
-
-
-
-    
-// function timer2(){
-//     let second = 0;
-
-//     const timer = setInterval(() => {
-
-//         let numberUtente = [];
-//         let utente = [];
-//         for (let i = 0; i < 5; i++) {
-            
-//             utente = parseInt(prompt('inserisci un numero'));
-//             numberUtente += utente;
-//             console.log(numberUtente[i]);
-            
-//         }
-        
-        
-//         for (let i = 0; i < 5; i++) {
-//             if(!(numberUtente[i] == numberRandom[i])){
-//                 console.log('sbagliato');
-//             }
-            
-//         }
-
-        
-        
-//     }, 1000);
-// }
